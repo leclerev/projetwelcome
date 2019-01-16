3da3bf9381fcd7756a6de88be613ee220ae438c2
leclerev <valentin.leclere@outlook.fr>
2019-01-16 13:41:44 +0100

    Adding Address Class

---
+++ src/Entity/Address.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AdresseRepository")
+ */
+class Address
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $street2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $zipCode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStreet1(): ?string
+    {
+        return $this->street1;
+    }
+
+    public function setStreet1(string $street1): self
+    {
+        $this->street1 = $street1;
+
+        return $this;
+    }
+
+    public function getStreet2(): ?string
+    {
+        return $this->street2;
+    }
+
+    public function setStreet2(?string $street2): self
+    {
+        $this->street2 = $street2;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(int $zipCode): self
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
