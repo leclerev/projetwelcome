2720773f1da0b3f7505b8b4f639299152710d7a8
leclerev <valentin.leclere@outlook.fr>
2019-01-18 13:35:58 +0100

    Fix API problems

--- src/Controller/GoodController.php
+++ src/Controller/GoodController.php
@@ -9,9 +9,6 @@ use App\Repository\GoodRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class GoodController extends AbstractController
 {
