9ba9b8598f1ad80cfde0dd7dea7ea9e94c9faa65
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-18 10:29:04 +0100

    Add login feature with email and password

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -162,7 +162,6 @@ class User implements UserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
@@ -171,7 +170,6 @@ class User implements UserInterface
 
     public function setRoles(array $roles): self
     {
-        $this->roles = $roles;
 
         return $this;
     }
