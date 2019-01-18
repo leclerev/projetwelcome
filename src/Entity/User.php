a3d7020edbe9de6c8ad00525b2e155dcc7819375
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-18 11:11:26 +0100

    Rename UtilisateurRepository to UserRepository

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
