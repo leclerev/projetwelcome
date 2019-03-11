25fd2464d74a3993fc3bb55cc1b6471bdb5fd902
digital <digital>
2019-03-11 14:58:51 +0100

    Entity Visitor created

---
+++ src/Entity/Visitor.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: digital
+ * Date: 2019-03-11
+ * Time: 14:19
+ */
+
+namespace App\Entity;
+
+use GraphAware\Neo4j\OGM\Annotations as OGM;
+
+/**
+ * @OGM\Node(label="Visitor")
+ */
+class NodeVisitor
+{
+    /**
+     * @OGM\GraphId()
+     */
+    protected $id;
+
+    /** @OGM\Property(type="string") */
+    protected $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+
+}
